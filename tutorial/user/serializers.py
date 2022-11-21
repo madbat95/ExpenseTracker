bd2ead9ea9cc9709dacfd5fa77555713ecd2f61a
hammad <hamahmed95@gmail.com>
2022-11-21 12:34:43 +0500

    changed user views to APIViews. the abstraction prevoiusly used has npw been replaced with custom made methods to perform create, read, update and delete users. additionally, another serializer has been added in the user to display accounts associated with users in UserDetail view only

--- tutorial/user/serializers.py
+++ tutorial/user/serializers.py
@@ -4,12 +4,37 @@ from rest_framework import serializers
 from account.models import Account
 from django.contrib.auth import get_user_model
 
+from account.serializers import AccountSerializer
+
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    accounts = serializers.PrimaryKeyRelatedField(many=True, queryset=Account.objects.all()) 
+    #accounts = AccountSerializer(many=True) #, queryset=Account.objects.all()) 
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'name',
-                  'dob', 'accounts']
+        fields = ('id', 'username', 'email', 'name',
+                  'dob')
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'username': {'read_only': True},
+            'email': {'read_only': True},
+            'name': {'read_only': True},
+            'dob': {'read_only': False},
+        }
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    accounts = AccountSerializer(many=True) #, queryset=Account.objects.all()) 
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'name',
+                  'dob', 'accounts')
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'username': {'read_only': True},
+            'email': {'read_only': True},
+            'name': {'read_only': True},
+            'dob': {'read_only': False},
+            'accounts': {'read_only': True},
+        }
